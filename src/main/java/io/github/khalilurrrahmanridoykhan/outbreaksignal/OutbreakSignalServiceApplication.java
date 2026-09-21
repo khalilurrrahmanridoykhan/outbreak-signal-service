dae8d959560ffaae7ac5cc67082f314ba4bb5190
khalilurrrahmanridoykhan <khalilurrahmanridoykhan@gmail.com>
2026-09-21 10:27:41 +0600

    build: scaffold Spring Boot 4.1 project on Java 25 with Maven

---
+++ src/main/java/io/github/khalilurrrahmanridoykhan/outbreaksignal/OutbreakSignalServiceApplication.java
@@ -0,0 +1,13 @@
+package io.github.khalilurrrahmanridoykhan.outbreaksignal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutbreakSignalServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OutbreakSignalServiceApplication.class, args);
+    }
+
+}
